23e74d72f4a7af4760440714ee371f652c652fa5
Deathnoval <badao867@gmail.com>
2023-03-29 12:18:49 +0700

    29/3

--- Car-Renting/fCar.cs
+++ Car-Renting/fCar.cs
@@ -27,6 +27,18 @@ namespace Car_Renting
         {
             this.gvCars.DataSource = carDao.GetAllDataTable();
         }
+        private void SearchProducts(string searchKeyword)
+        {
+            DataTable table = new DataTable();
+            table = carDao.GetAllDataTable();
+            var result = from row in table.AsEnumerable()
+                         where row.Field<string>("CarName").ToLower().Contains(searchKeyword.ToLower()) || row.Field<string>("CategoryName").ToLower().Contains(searchKeyword.ToLower())
+                         select row;
+            this.gvCars.DataSource = result.CopyToDataTable();
+
+        }
+
+        
 
         private void gvCars_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -62,6 +74,65 @@ namespace Car_Renting
                 form.OpenChildForm(new fRent(currentForm,car));
                 form.DisableButton();
                 form.leftBorderBtn.Visible = false; ;
+            }
+        }
+
+        private void btTim_Click_1(object sender, EventArgs e)
+        {
+
+            string searchKeyword = txtTimXe.Text.Trim();
+            SearchProducts(searchKeyword);
+        }
+
+        private void comboxLoaiXe_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string LoaiXe = comboxLoaiXe.SelectedItem.ToString();
+
+            // Create a binding source object to filter the data source of the DataGridView
+            BindingSource bs = new BindingSource();
+            bs.DataSource = gvCars.DataSource;
+
+            // Apply a filter to the binding source based on the selected type of vehicle
+            // If the user selects "All", show all vehicles
+            if (LoaiXe == "All")
+            {
+                bs.Filter = "";
+            }
+            else
+            {
+                bs.Filter = "CategoryName = '" + LoaiXe + "'";
+            }
+
+            // Set the data source of the DataGridView to the binding source
+            gvCars.DataSource = bs;
+        }
+
+        private void comboxTieuChi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboxTieuChi.SelectedItem.ToString() == "Xe được thuê nhiều")
+            {
+                Dictionary<int, int> carRentalCounts = new Dictionary<int, int>();
+
+                // Lặp qua từng dòng trong datagridview
+                foreach (DataGridViewRow row in gvCars.Rows)
+                {
+                    int carModel = (int)row.Cells["CarId"].Value;
+                    int carCounts = (int)row.Cells["so_luong_thue"].Value;
+
+                    // Tăng giá trị tương ứng của xe đó trong Dictionary
+
+
+                    carRentalCounts.Add(carModel, carCounts);
+
+                }
+
+                // Lấy 3 xe được thuê nhiều nhất
+                var top3CarModels = carRentalCounts.OrderByDescending(x => x.Value).Take(3);
+
+            }
+            if (comboxTieuChi.SelectedItem.ToString() == "Xe được được đánh giá cao")
+            {
+
             }
         }
     }
